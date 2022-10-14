3c1ec6c0689cc53a0d6f2aea7d622e88bcb130a8
grishanova-oa <grishanova.oa@gmail.com>
2022-10-14 09:45:39 +0200

    feat: done task

--- src/simple-chain.js
+++ src/simple-chain.js
@@ -4,28 +4,61 @@ const { NotImplementedError } = require('../extensions/index.js');
  * Implement chainMaker object according to task description
  * 
  */
-const chainMaker = {
+ const chainMaker = {
+  chain: [],
   getLength() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+    return this.chain.length;
   },
-  addLink(/* value */) {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+  addLink(value) {
+    if (value === undefined) {
+      this.chain.push('(  )');
+      return this;
+    }
+    if (value === null) {
+      this.chain.push(value);
+      return this;
+    }
+    this.chain.push(value.toString());
+
+    return this;
   },
-  removeLink(/* position */) {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+  removeLink(position) {
+    if (
+      typeof position == 'number' &&
+      position % 1 == 0 &&
+      position > 0 &&
+      this.chain.length >= position
+    ) {
+      this.chain.splice([position - 1], 1);
+
+      return this;
+    }
+    else {
+      this.chain =[]; 
+      throw new Error("You can\'t remove incorrect link!")
+     }
   },
   reverseChain() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+    this.chain.reverse();
+
+    return this;
   },
   finishChain() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+    let result = '';
+    this.chain.forEach((item, index) => {
+      if (index + 1 === this.chain.length) {
+        result += `( ${item} )`;
+      } else {
+        result += `( ${item} )~~`;
+      }
+
+    })
+
+    return result;
   }
 };
+
+
 
 module.exports = {
   chainMaker
