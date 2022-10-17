bb8712602198c479a5dae3e2d423a2dc5df0a195
grishanova-oa <grishanova.oa@gmail.com>
2022-10-17 23:54:59 +0200

    feat: add solution

--- src/extended-repeater.js
+++ src/extended-repeater.js
@@ -15,9 +15,33 @@ const { NotImplementedError } = require('../extensions/index.js');
  * => 'STRINGPLUS00PLUS00PLUS**STRINGPLUS00PLUS00PLUS**STRINGPLUS00PLUS00PLUS'
  *
  */
-function repeater( str, options ) {
+ function repeater(str, options) {
+  let arr = [str];
+  let newString = [];
+  let newArr = [];
+  let {
+    repeatTimes = 1,
+    separator = '+',
+    addition = '',
+    additionRepeatTimes = 1,
+    additionSeparator = '|' } = options;
+  if (addition === null) {
+    addition = 'null';
+  }
 
+
+  for (let i = 0; i < repeatTimes; i++) {
+    for (let j = 0; j < additionRepeatTimes; j++) {
+      newString.push(addition);
+      if (j == additionRepeatTimes - 1) {
+        newArr.push(arr[0] + newString.join(additionSeparator));
+        newString = [];
+      }
+    }
+  }
+  return newArr.join(separator);
 }
+
 
 module.exports = {
   repeater
